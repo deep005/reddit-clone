d1c27f4e2965e889d865651a8d30c712e9fcd886
Deep Shanker <deep.guha@traveltripper.com>
2017-05-04 01:27:58 +0530

    hitting the api

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: [5, 'Username must be minimum of 5 characters']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'Password must be minimum of 8 characters']
+    }
+});
+
+//Write some encryption for Password
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
